14a7d0a58f14fb0ce8c244058cbde4e01adfff78
natscott1738 <natebaraka777@gmail.com>
2024-10-01 21:50:28 +0000

    ES6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-	let task = false;
- 	let task2 = true;
+	const task = false;
+ 	const task2 = true;
 
   if (trueOrFalse) {
-	  let blockTask = true;
-	  let blockTask2 = false;
-
-	  return [blockTask, blockTask2];
+	  const task = true;
+	  const task2 = false;
+	  return [task, task2];
   }
 
   return [task, task2];
