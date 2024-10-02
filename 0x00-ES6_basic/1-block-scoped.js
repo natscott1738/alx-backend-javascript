4b4724e4c850f947611524923bfba00202cf4b5e
natscott1738 <natebaraka777@gmail.com>
2024-10-02 08:13:06 +0000

    ES6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
 	const task2 = true;
 
 	if (trueOrFalse) {
-		const task = true; // eslint error no un-used vars
-		const task2 = false; // eslint error no un-used vars
+		// const task = true; // eslint error no un-used vars
+		// const task2 = false; // eslint error no un-used vars
 	}
 
 	return [task, task2];
