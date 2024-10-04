dcce593edbc7ae0a75b972532d0b2bd1626c4c50
natscott1738 <natebaraka777@gmail.com>
2024-10-04 13:59:40 +0000

    ES6-promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
-      console.log('Got a response from API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => new Error());
+  return promise.then(() => (
+    { status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
