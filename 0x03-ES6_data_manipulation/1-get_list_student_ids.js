4b3da0a50ec78eca5aee9eab7f94e787155cf4f4
natscott1738 <natscott383@gmail.com>
2024-10-07 18:37:48 +0300

    es6 data manip

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,7 @@ function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  return students.map(student => student.id);
+  return students.map((student) => student.id);
 }
 
 export default getListStudentIds;
