17a9d498496ec496552121bdb765ae805a480714
natscott1738 <natscott383@gmail.com>
2024-10-08 16:33:24 +0300

    es6 data manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+function cleanSet(set, startString) {
+  if (startString === '') return '';
+
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
