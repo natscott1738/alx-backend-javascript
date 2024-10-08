bc177d1b09db5fef396e3bb16d77feeca2960e67
natscott1738 <natscott383@gmail.com>
2024-10-08 17:04:25 +0300

    es6 data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,11 @@
-function cleanSet(set, startString) {
-  if (startString === '') return '';
+export default function cleanSet(set, str) {
+  if (!str || !str.length) return '';
+  let values = '';
 
-  return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
+  for (const i of set) {
+    if (i && i.startsWith(str)) {
+      values += values.length === 0 ? i.replace(str, '') : i.replace(str, '-');
+    }
+  }
+  return values;
 }
-
-export default cleanSet;
